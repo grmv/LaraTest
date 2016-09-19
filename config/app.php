82690617a91fc962bc6465257fc079d47174e07c
Jumbie <gromov.tema@gmail.com>
2016-09-19 16:53:29 +0600

    дефолтный лейоут и инклюды

--- config/app.php
+++ config/app.php
@@ -178,6 +178,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Мои
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
     ],
 
     /*
